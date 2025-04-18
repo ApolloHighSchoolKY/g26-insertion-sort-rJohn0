46720e40566ef261a370fb8c4f01280469fd1740
rJohn0 <romeo.john@stu.daviess.kyschools.us>
2025-04-18 17:34:59 +0000

    done

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -1,9 +1,25 @@
+import java.util.Arrays;
+
 public class AInsertionSort
 {
 
     public static void main(String[] args)
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
+        int temp;
+        for(int i =1; i<myNumbers.length;i++)
+        {
+            for(int j =i; j>0; j--)
+            {
 
+                if(myNumbers[j] <myNumbers[j-1])
+                {
+                    temp = myNumbers[j];
+					myNumbers[j] = myNumbers[j - 1];
+					myNumbers[j-1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(myNumbers));
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ALInsertionSort
 {
@@ -9,26 +10,41 @@ public class ALInsertionSort
     	ArrayList<Integer> sorted = new ArrayList<Integer>();
     	boolean inserted=false;
 
+		System.out.println("Before: " + Arrays.toString(myNumbers));
 		//Add the first number to the array list
-
+		sorted.add(myNumbers[0]);
 		//Loop once for all of the remaining numbers in the unsorted list
-
+		for(int i =1; i <myNumbers.length;i++)
+		{
+			inserted = false;
 			//Check with each of the numbers in the sorted list
-
+			for(int j=0; j<sorted.size();j++)
+			{
 				//If this number is less than one in the sorted list,
 				//insert it there
+				if(myNumbers[i] <= sorted.get(j))
+				{
+					sorted.add(j, myNumbers[i]);
+					inserted = true;
+					// Only way i know how to stop loop other hthen break
+					j = sorted.size();
+				}
 
+			}
 			//If it was not inserted, stick it on the end.
-
-
+			
+			if(!inserted)
+				sorted.add(myNumbers[i]);
+		}
 		//End Loop for unsorted list
 
 		System.out.println(sorted);
 
 		//Move the data back to the array
-
+		for(int i = 0; i<myNumbers.length-1; i++)
+			myNumbers[i] = sorted.get(i);
 		//Print the contents of the array
-
+		System.out.println("After: " + Arrays.toString(myNumbers));
     }
 
 
